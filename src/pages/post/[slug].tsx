5033b018f06cc004eda91c416939ea63eef46858
rubensmk <rubenskishimoto@gmail.com>
2021-08-23 09:46:52 -0300

    Add Prev/Next Post, Comment Section and Edited Post Message

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -21,9 +21,11 @@ import Header from '../../components/Header';
 import { getPrismicClient } from '../../services/prismic';
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import Comments from '../../components/Comments';
 
 interface Post {
   first_publication_date: string | null;
+  last_publication_date: string | null;
   data: {
     title: string;
     banner: {
@@ -41,10 +43,28 @@ interface Post {
 
 interface PostProps {
   post: Post;
+  navigation: {
+    prevPost: {
+      uid: string;
+      data: {
+        title: string;
+      };
+    }[];
+    nextPost: {
+      uid: string;
+      data: {
+        title: string;
+      };
+    }[];
+  };
   preview: boolean;
 }
 
-export default function Post({ post, preview }: PostProps): JSX.Element {
+export default function Post({
+  post,
+  preview,
+  navigation,
+}: PostProps): JSX.Element {
   const totalWords = post?.data.content.reduce((total, contentItem) => {
     total += contentItem.heading?.split(' ').length;
 
@@ -70,6 +90,21 @@ export default function Post({ post, preview }: PostProps): JSX.Element {
       locale: ptBR,
     }
   );
+
+  const isPostEdited =
+    post.first_publication_date !== post.last_publication_date;
+
+  let editionDate;
+
+  if (isPostEdited) {
+    editionDate = format(
+      new Date(post.last_publication_date),
+      "'* editado em' dd MM yyyy', às' H':'mm'",
+      {
+        locale: ptBR,
+      }
+    );
+  }
   return (
     <>
       <Head>
@@ -98,6 +133,10 @@ export default function Post({ post, preview }: PostProps): JSX.Element {
             </p>
           </div>
 
+          <div className={styles.edited}>
+            {isPostEdited && <span>{editionDate}</span>}
+          </div>
+
           <div className={styles.body}>
             {post?.data.content.map(content => {
               return (
@@ -114,6 +153,31 @@ export default function Post({ post, preview }: PostProps): JSX.Element {
             })}
           </div>
         </section>
+
+        <section className={`${styles.navigation}`}>
+          {navigation?.prevPost.length > 0 && (
+            <div>
+              <h3>{navigation.prevPost[0].data.title}</h3>
+              <Link href={`/post/${navigation.prevPost[0].uid}`}>
+                <a>Post anterior</a>
+              </Link>
+            </div>
+          )}
+
+          {navigation?.nextPost.length > 0 && (
+            <div>
+              <h3>{navigation.nextPost[0].data.title}</h3>
+              <Link href={`/post/${navigation.nextPost[0].uid}`}>
+                <a>Próximo post</a>
+              </Link>
+            </div>
+          )}
+        </section>
+
+        <div className={styles.comments}>
+          <h2>Comentários</h2>
+          <Comments />
+        </div>
 
         {preview && (
           <aside>
@@ -158,9 +222,28 @@ export const getStaticProps: GetStaticProps = async ({
     ref: previewData?.ref || null,
   });
 
+  const prevPost = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.first_publication_date]',
+    }
+  );
+
+  const nextPost = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.last_publication_date desc]',
+    }
+  );
+
   const post = {
     uid: response.uid,
     first_publication_date: response.first_publication_date,
+    last_publication_date: response.last_publication_date,
     data: {
       title: response.data.title,
       subtitle: response.data.subtitle,
@@ -180,6 +263,10 @@ export const getStaticProps: GetStaticProps = async ({
   return {
     props: {
       post,
+      navigation: {
+        prevPost: prevPost?.results,
+        nextPost: nextPost?.results,
+      },
       preview,
     },
   };
